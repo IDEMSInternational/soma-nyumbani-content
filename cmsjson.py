a1bc6becda759b73c0d5bc72d31d3921f14fe978
John Argentino <john.argentino@outlook.com>
2020-09-02 13:58:36 -0400

    splitting string by "h1"

--- cmsjson.py
+++ cmsjson.py
@@ -1,21 +1,15 @@
 #reads an csm html file and converts it into a json file in which the headings are converted into elements and the remaining html is preserved
-
-print("hello")
-
 squares = open('squares.html','r')
-json = {
-    "name":"John",
-    "type":"human",
-
-
-
-
-}
-linelist = []
+html_str = ""
 for line in squares:
-    linelist.append(line)
+    html_str = html_str + line
+    
+h1split = html_str.split("h1")
+for x in h1split:
+    print(x)
 
 
-print(json["name"])  
+
+
 
 
