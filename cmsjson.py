d68083258213fb1117341e91576790d5da964acb
John Argentino <john.argentino@outlook.com>
2020-09-03 17:04:32 -0400

    isolated headers and subheaders

--- cmsjson.py
+++ cmsjson.py
@@ -7,20 +7,35 @@ for line in squares:
 json = {}
 
 h1split = html_str.split("<h1><span>")
-count = 0
+count1 = 0
 for x in h1split:
-    if count == 0:
-        print(count)   #code to identify document titles
+    if count1 == 0:
+        print("later")
+          #code to identify document titles
     else:
-        index = x.index("<")
-        title = x[0:index] + "--"
-        #json[x[:index]= "hi"]
-        print(title)
-    count = count + 1
+        
+        index1 = x.index("<")
+        head1 = x[0:index1]
+        tail1 = x[index1:]
+        #nested process for isolating header2's
+        h2split = tail1.split("<h2><span>")
+        count2 = 0
+        print(head1)
+        print("")
+        for y in h2split:
+            index2 = y.index("<")
+            head2 = y[0:index2]
+            tail2 = y[index2:]
+            print(head2)
+            json[head1] = head2
+
+        #json[head1]= tail1 (values of h1's are just html script)
+            count2 = count2 + 1
+        
+        print("")
+        print("")
+        print("")
+
+    count1 = count1 + 1
     
-    
-
-
-
-
 
