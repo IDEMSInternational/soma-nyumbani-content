8d584d98dda82522f35c07f8a5348b39fd886e10
John Argentino <john.argentino@outlook.com>
2020-09-08 13:12:34 -0400

    stripped "Activity: " from main dict keys (and added some comments0

--- cmsjson.py
+++ cmsjson.py
@@ -24,12 +24,19 @@ for x in h1split:
         
         index1 = x.index("</span></h1>")
         head1 = x[0:index1]
+        #specific measures need to be taken for Activities, namely stripping out the "Activity: " 
+        # portion of the header, and (as of now) making Activity its type
+        act = "Activity: "
+        if head1.find(act) != -1:
+            head1 = x[len(act):index1]
+            json[head1] = {}
+            json[head1]["type"] = "Activity"
+        else:
+            json[head1] = {}
         tail1 = x[index1:]
         #print(head1)
         #nested process for isolating header2's
         h2split = tail1.split("<h2><span>")
-        count2 = 0
-        json[head1] = {}
         #add entry to json dict that has h1 as a key and an open dict (to be filled with h2 and corresponding html) as value 
         count2 = 0
         for y in h2split:
@@ -37,7 +44,7 @@ for x in h1split:
                 index2 = y.index("<")
                 head2 = y[0:index2]
                 tail2 = y[index2:]
-                json[head1][head2]=tail2[:5]
+                json[head1][head2]=tail2
                 #print(head2)
             count2 = count2 + 1   
 
@@ -50,11 +57,10 @@ for x in h1split:
     #print("")
     #print("")
     #print("")
-
-print(json["name"])
-print(json["type"])
-print(json["Session Outline"])
+for key in json.keys():
+    print(key)
 
 
 
-#remains to be done: altering "Activity Guide: ..." to just have the title, cropping html values (or not?)
+
+#remains to be done: altering "Activity Guide: 
